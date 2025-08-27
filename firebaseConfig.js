b7dfe162a37d6e388283447088a547889b37b365
ShanelleDejito <jamesivandelector06@gmail.com>
2025-08-27 15:34:09 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAn3uAyWQInAH-kkbU0Qnvrc_21axvnH78",
+  authDomain: "daily-habit-2004.firebaseapp.com",
+  projectId: "daily-habit-2004",
+  storageBucket: "daily-habit-2004.firebasestorage.app",
+  messagingSenderId: "872163122644",
+  appId: "1:872163122644:web:a0c8e561f75e7b570eca41"
+};
+
+const app = initializeApp(firebaseConfig);
